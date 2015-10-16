9410d9095e61010a33113ba0721004f79bf67f8c
foocoder <slow295185031@gmail.com>
2015-10-16 21:55:42 +0800

    add nester.py

---
+++ nester/nester.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#FileName: nester.py
+#Author:   Fuchen Duan
+#Email:    slow295185031@gmail.com
+
+def print_lol(the_list, indent=False, level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1);
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t'),
+            print(each_item);
